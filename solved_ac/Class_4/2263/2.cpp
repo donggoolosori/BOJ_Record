2fefd3f6d7de4d817437d8c86e440c19a61c3c8e
donggoolosori <kdj6551@naver.com>
2021-05-15 18:36:09 +0900

    Solve 2263 #2

--- solved_ac/Class_4/2263/2.cpp
+++ solved_ac/Class_4/2263/2.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
-#include <vector>
+#define MAX 100001
 
 using namespace std;
 
 int N;
-vector<int> in, post;
+int indexs[MAX], in[MAX], post[MAX];
+
+void printPreOrder(int is, int ie, int ps, int pe)
+{
+    if (is > ie || ps > pe)
+        return;
+    int mid = indexs[post[pe]];
+    cout << in[mid] << ' ';
+
+    int l_size = mid - is, r_size = ie - mid;
+
+    // 왼쪽 서브트리 탐색
+    printPreOrder(is, mid - 1, ps, ps + l_size - 1);
+    // 오른쪽 서브트리 탐색
+    printPreOrder(mid + 1, ie, ps + l_size, pe - 1);
+}
 
 int main()
 {
@@ -14,17 +29,15 @@ int main()
 
     cin >> N;
 
-    int n;
     for (int i = 0; i < N; i++)
     {
-        cin >> n;
-        in.push_back(n);
+        cin >> in[i];
+        indexs[in[i]] = i;
     }
     for (int i = 0; i < N; i++)
-    {
-        cin >> n;
-        post.push_back(n);
-    }
+        cin >> post[i];
+
+    printPreOrder(0, N - 1, 0, N - 1);
 
     return 0;
 }

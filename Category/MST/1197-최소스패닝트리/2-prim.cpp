63a021ba3bc09847567befb0a29beb77f272f566
donggoolosori <kdj6551@naver.com>
2021-06-26 19:06:13 +0900

    solve: use prim

---
+++ Category/MST/1197-최소스패닝트리/2-prim.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+  int idx, cost;
+  bool operator<(const Node &b) const { return cost > b.cost; }
+};
+vector<Node> graph[10001];
+int visit[10001];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int answer = 0;
+
+  int V, E;
+  cin >> V >> E;
+
+  int a, b, c;
+  while (E--) {
+    cin >> a >> b >> c;
+    graph[a].push_back({b, c});
+    graph[b].push_back({a, c});
+  }
+
+  visit[1] = true;
+  priority_queue<Node> pq;
+  for (const auto &el : graph[1]) {
+    pq.push(el);
+  }
+
+  while (!pq.empty()) {
+    int idx = pq.top().idx, cost = pq.top().cost;
+    pq.pop();
+
+    if (visit[idx]) continue;
+    visit[idx] = true;
+    answer += cost;
+
+    for (const auto &next : graph[idx]) {
+      if (visit[next.idx]) continue;
+      pq.push(next);
+    }
+  }
+
+  cout << answer << '\n';
+
+  return 0;
+}

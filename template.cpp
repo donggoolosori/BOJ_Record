e284d2a6e49a72802bb7b59fffde7fd414240fee
donggoolosori <kdj6551@naver.com>
2021-12-04 18:18:42 +0900

    Add FAST_IO macro

--- template.cpp
+++ template.cpp
@@ -4,14 +4,17 @@
 #include <string>
 #include <vector>
 
-#define FOR(i, s, e) for (int i = s; i < e; i++
+#define FOR(i, s, e) for (int i = s; i < e; i++)
 #define INF 987654321
+#define FAST_IO                     \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
 
 using namespace std;
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+  FAST_IO;
+
   return 0;
 }

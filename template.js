df08a46e30b7e4864e5a35053e17c075ea406cc4
donggoolosori <kdj6551@naver.com>
2022-02-17 13:38:09 +0900

    txt 파일 읽는 코드 추가

--- template.js
+++ template.js
@@ -1,4 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').split('\n');
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
+// const input = fs.readFileSync(0,'utf-8').split('\n');
+
+console.log(input);

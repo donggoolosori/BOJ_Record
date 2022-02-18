2f49f4a1faa09126765629b77d93e4bec4848364
donggoolosori <kdj6551@naver.com>
2022-02-18 16:25:17 +0900

    template 수정

--- template.js
+++ template.js
@@ -1,9 +1,9 @@
 'use strict';
-
 const fs = require('fs');
+
+function solution(input) {}
+
 // const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
 const input = fs.readFileSync(0, 'utf-8').split('\n');
 
-function solution() {}
-
-solution();
+solution(input);

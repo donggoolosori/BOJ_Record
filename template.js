150b9a3638d21b75fbaf33cf21410b69cd8e0e1c
donggoolosori <kdj6551@naver.com>
2022-10-10 23:20:28 +0900

    feat: Update template.js

--- template.js
+++ template.js
@@ -1,20 +1,26 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
+"use strict";
+const fs = require("fs");
+const path = require("path");
 
-function solution(input) {}
+function solution() {}
+
+function refineInput(input) {
+  // const [n, m] = input[0].split(" ").map(Number);
+  // const paper = input.slice(1, 1 + n).map((s) => s.split(" ").map(Number));
+  // return [n, m, paper];
+}
 
 function printAnswer(ans) {
-  console.log(ans);
+  // console.log(ans);
 }
 
 function input() {
   const read = fs
-    .readFileSync(path.join(__dirname, '/input/input-0.txt'), 'utf-8')
-    .split('\n');
+    .readFileSync(path.join(__dirname, "/input/input-0.txt"), "utf-8")
+    .split("\n");
   // const read = fs.readFileSync(0, 'utf-8').split('\n');
 
-  return [read];
+  return read;
 }
 
-printAnswer(solution(...input()));
+printAnswer(solution(...refineInput(input())));

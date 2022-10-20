81cfcf5f6e85d43be97a4c4b11e94c37f865c7bb
donggoolosori <kdj6551@naver.com>
2022-10-20 23:43:00 +0900

    wip

--- Category/Graph/1707-이분 그래프/solution.js
+++ Category/Graph/1707-이분 그래프/solution.js
@@ -3,13 +3,24 @@ const fs = require("fs");
 const path = require("path");
 
 function refineInput(input) {
-  // const [n, m] = input[0].split(" ").map(Number);
-  // return [n, m];
+  const t = Number(input[0]);
+  const inputs = input.slice(1, -1).map((s) => s.split(" ").map(Number));
+
+  return [t, inputs];
 }
 
-function solution(n, m) {
-  // const ans = n+m;
-  // return ans;
+function solution(t, inputs) {
+  let read = 0;
+  while (t--) {
+    const [v, e] = inputs[read];
+    const graph = Array.from({ length: v + 1 }, () => Array.from({ length: v + 1 }, () => false));
+    for (let i = read + 1; i <= read + e; i++) {
+      const [u, v] = inputs[i];
+      graph[u][v] = true;
+      graph[v][u] = true;
+    }
+    read += e + 1;
+  }
 }
 
 function printAnswer(ans) {
@@ -17,9 +28,7 @@ function printAnswer(ans) {
 }
 
 function input() {
-  const read = fs
-    .readFileSync(path.join(__dirname, "/input/input-0.txt"), "utf-8")
-    .split("\n");
+  const read = fs.readFileSync(path.join(__dirname, "/input/input-0.txt"), "utf-8").split("\n");
   // const read = fs.readFileSync(0, 'utf-8').split('\n');
 
   return read;

3e4247ccc38634a8a7465516849e9a9b09e1ccd8
donggoolosori <kdj6551@naver.com>
2022-11-17 23:02:23 +0900

    solve 11508

--- 11508-2+1 세일/solution.js
+++ 11508-2+1 세일/solution.js
@@ -3,23 +3,30 @@ const fs = require("fs");
 const path = require("path");
 
 function refineInput(input) {
-  // const [n, m] = input[0].split(" ").map(Number);
-  // return [n, m];
+  const n = Number(input[0]);
+  const prices = input.slice(1, 1 + n).map(Number);
+
+  return [n, prices];
 }
 
-function solution(n, m) {
-  // const ans = n+m;
-  // return ans;
+function solution(n, prices) {
+  prices.sort((a, b) => b - a);
+
+  let ret = 0;
+  for (let i = 0; i < n; i += 3) {
+    ret += prices[i];
+    if (prices[i + 1]) ret += prices[i + 1];
+  }
+
+  return ret;
 }
 
 function printAnswer(ans) {
-  // console.log(ans);
+  console.log(ans);
 }
 
 function input() {
-  const read = fs
-    .readFileSync(path.join(__dirname, "/input/input-0.txt"), "utf-8")
-    .split("\n");
+  const read = fs.readFileSync(path.join(__dirname, "/input/input-1.txt"), "utf-8").split("\n");
   // const read = fs.readFileSync(0, 'utf-8').split('\n');
 
   return read;
